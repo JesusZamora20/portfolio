108fa142ef216ddcbeb86a00a0f6942ebbabc18e
JesusZamora20 <jbermudezamora87@gmail.com>
2023-03-02 00:33:17 -0500

    aligning items to center

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,11 +1,11 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 import './projects.css'
 
 function Projects() {
     return (
         <Box className='projects-box'>
-
+            <Typography variant='h1' id='skills-title' >Projects</Typography>
         </Box>
     );
 }
